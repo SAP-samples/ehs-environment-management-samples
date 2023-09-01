47afa65567c7bd896ad2b49663b0162075aea611
Uwe Klinger <uwe.klinger@sap.com>
2023-09-01 13:00:52 +0200

    Work from main directory

--- tools/migration-data-creator-cli.js
+++ tools/migration-data-creator-cli.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { XMLParser, XMLBuilder } = require('fast-xml-parser');
 
-eval(fs.readFileSync('./DataMigrationUtil.js', 'utf-8'));
+eval(fs.readFileSync('./tools/DataMigrationUtil.js', 'utf-8'));
 
-const config = fs.readFileSync("../greenhouse-gas-management/Example Config.xml", "utf-8");
-console.log(readExcelSimple(config));
+const config = fs.readFileSync("./greenhouse-gas-management/Example Config.xml", "utf-8");
+console.log(JSON.stringify(readExcelSimple(config), null, 2));
